662a2e9b4bc7277dd1a9e168f2b3f95e954eeaa4
N1C0L4Ssilva <nicolassipva3317@gmail.com>
2023-12-19 21:08:48 -0300

    Funções da pagina lendo

---
+++ src/app/Historias.ts
@@ -0,0 +1,23 @@
+export function TesteTexto(){return"\
+Numa pequena cidade, onde as ruas eram estreitas e as casas contavam histórias antigas, vivia uma comunidade unida.\
+    Cada manhã, o sol surgia pintando o céu com tons suaves, enquanto os moradores começavam seu dia.\
+    Joana, uma jovem apaixonada por livros, trabalhava na pequena livraria da esquina. Seu refúgio era o mundo das palavras, onde personagens ganhavam vida e aventuras se desenrolavam a cada página virada.\
+    Nas tardes de domingo, a praça central se enchia de risos e conversas animadas, criando laços que perduravam por gerações. No parque, as crianças corriam pelos gramados, empinando pipas e soltando risos que ecoavam.\
+    Os idosos se reuniam nos bancos, compartilhando memórias e sabedoria acumulada ao longo dos anos. As fachadas das casas contavam a história da cidade, com arquitetura que resistia ao tempo e testemunhava mudanças sutis.\
+    Cada estação do ano trazia sua própria magia:\
+    a primavera pintava a cidade com cores vibrantes,\
+    o verão trazia noites estreladas e festivais na praça,\
+    o outono cobria as ruas com folhas douradas,\
+    e o inverno transformava a paisagem em um cenário de conto de fadas.\
+    Assim, essa pequena cidade seguia seu ritmo tranquilo, onde a simplicidade cotidiana se revelava em gestos de bondade, sorrisos compartilhados e o calor de uma comunidade que encontrava alegria nos detalhes mais simples da vida."}
+
+
+export function TresPorquinhos(){return"\
+Era uma vez três porquinhos chamados Prático, Heitor e Cícero, que decidiram construir suas próprias casas. O primeiro porquinho, Prático, era muito apressado e construiu sua casa de palha. Ele queria terminar logo para poder brincar. \
+    O segundo porquinho, Heitor, escolheu construir sua casa de madeira. Ele queria algo um pouco mais resistente, mas ainda assim, queria aproveitar a vida e se divertir. \
+    O terceiro porquinho, Cícero, era o mais trabalhador dos três. Ele decidiu construir uma casa de tijolos, pois sabia que isso ofereceria a melhor proteção contra o lobo mau. \
+    Um dia, o lobo mau apareceu na floresta e viu as casas dos porquinhos. Ele primeiro foi até a casa de palha e, com um sopro forte, derrubou-a rapidamente. Prático teve que correr para a casa de madeira do Heitor. \
+    O lobo, determinado, foi até a casa de madeira e, com um sopro forte, conseguiu derrubá-la também. Agora, os dois porquinhos tiveram que correr para a casa de tijolos de Cícero. \
+    O lobo mau chegou à casa de tijolos e tentou derrubá-la com todos os seus sopros, mas ela permaneceu firme. Frustrado, o lobo desistiu e foi embora. Os três porquinhos aprenderam a importância de construir com cuidado e resistência, e viveram felizes em sua casa segura."}
+
+export function TextTTk(){return"Você sabia que você consegue ler quase 300 palavras por minuto? porem, sem precisar mexer os olhos você pode facilmente ler muito mas rapido por exemplo agora você esta lendo 400 palavras por minuto isso é incrivel e agora você esta a 500 palavras por minuto isso serve para mostrar o tanto que a mente humana é capaz de fazer coisas espetaculares"}
--- src/app/lendo/lendo.component.ts
+++ src/app/lendo/lendo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { TresPorquinhos,TesteTexto, TextTTk } from "../Historias"
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-lendo',
   standalone: true,
@@ -8,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrl: './lendo.component.css'
 })
 export class LENDOComponent {
+  private PPM_Min:number=100;
+  public PPM_Max:number=300;
+  public PPM_Atu:number=this.PPM_Min;
+  private SpeedUp:number=5;
 
+  public KeepReading:boolean=false
+  public UltimaLetra:number=0;
+
+  private Texto:string=TresPorquinhos()
+  public Plv_Atu:string="PRONTO?";
+  public TextoTratado:string[]=[];
+
+  constructor(public Route:Router){}
+  ngOnInit(){
+    this.TextoTratado=this.Texto.split(" ")
+  }
+  public ComecarLeitura= async ()=>{
+    this.PPM_Atu=this.PPM_Min
+    if(!this.KeepReading){
+      this.KeepReading=true
+      // CONTAGEM REGRESSIVA
+      for(let i=3;i>0;i--){
+        this.Plv_Atu=i.toString()
+        await new Promise(f => setTimeout(f,1000))
+      }
+      // INCIAR TEXTO
+      for(let i=this.UltimaLetra ;this.TextoTratado.length>i;i++){
+        this.UltimaLetra=i
+        this.Plv_Atu=this.TextoTratado[this.UltimaLetra]
+        await new Promise(f => setTimeout(f,60000/this.PPM_Atu))
+        if(this.PPM_Atu<this.PPM_Max){
+          if(this.PPM_Atu+this.SpeedUp>this.PPM_Max){
+            this.PPM_Atu=this.PPM_Max
+          }else{
+            this.PPM_Atu+=this.SpeedUp
+          }
+        }
+        if(!this.KeepReading){
+          break
+        }
+      }
+    }
+  }
+  public Pausar=()=>{
+    this.KeepReading=false
+  }
+  public Sair=()=>{
+    this.Route.navigate(["/"])
+  }
+  public Restart=()=>{
+    this.Pausar()
+    this.UltimaLetra=0
+    this.Plv_Atu="PRONTO?";
+  }
 }
